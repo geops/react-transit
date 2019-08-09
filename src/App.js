f9e2108cfee8542c54624d4fc291a21f0b15453c
Olivier Terral <olivier.terral@geops.de>
2019-08-09 10:47:22 +0200

    Use mapbox for the TrackerLayer

--- src/App.js
+++ src/App.js
@@ -1,26 +1,31 @@
 import React, { useState, useEffect } from 'react';
 import BasicMap from 'react-spatial/components/BasicMap';
-import Layer from 'react-spatial/Layer';
-import TileLayer from 'ol/layer/Tile';
-import OSMSource from 'ol/source/OSM';
+// import Layer from 'react-spatial/Layer';
+// import TileLayer from 'ol/layer/Tile';
+// import OSMSource from 'ol/source/OSM';
 import Footer from 'react-spatial/components/Footer';
 import DatePicker from 'react-datepicker';
 import TrackerControl from './components/TrackerControl';
-import TrackerLayer from './components/Tracker/TrackerLayer';
+// import TrackerLayer from './components/Tracker/TrackerLayer';
+import TrackerMapboxSource from './components/Tracker/TrackerMapboxSource';
 import Clock from './components/Clock';
 import LocalClock from './components/LocalClock';
 
 import './App.scss';
 import 'react-datepicker/dist/react-datepicker.css';
 
-const trackerLayer = new TrackerLayer();
+const trackerLayer = new TrackerMapboxSource({
+  name: 'Tracker',
+  url:
+    'https://maps.geops.io/styles/trafimage_perimeter_v2/style.json?key=5cc87b12d7c5370001c1d6557f01e26728174c1fa19d33afe303b910',
+});
 const layers = [
-  new Layer({
+  /* new Layer({
     name: 'Layer',
     olLayer: new TileLayer({
       source: new OSMSource(),
     }),
-  }),
+  }), */
   trackerLayer,
 ];
 
--- src/components/Tracker/Tracker.js
+++ src/components/Tracker/Tracker.js
@@ -1,3 +1,4 @@
+import { toLonLat } from 'ol/proj';
 /**
  * Tracker for OpenLayers.
  */
@@ -12,7 +13,7 @@ export default class Tracker {
     this.trajectories = [];
     this.rotationCache = {};
     this.renderFps = 60;
-
+    /*
     this.map.on('change:size', () => {
       [this.canvas.width, this.canvas.height] = this.map.getSize();
     });
@@ -32,7 +33,7 @@ export default class Tracker {
 
     this.map.once('postrender', () => {
       this.map.getTarget().appendChild(this.canvas);
-    });
+    }); */
   }
 
   setTrajectories(trajectories) {
@@ -167,6 +168,76 @@ export default class Tracker {
         this.removeTrajectory(traj.id);
       }
     }
+  }
+
+  renderGeojsonTrajectory(currTime = Date.now()) {
+    this.clear();
+    const geojson = {
+      type: 'FeatureCollection',
+      features: [],
+    };
+
+    for (let i = this.trajectories.length - 1; i >= 0; i -= 1) {
+      const traj = this.trajectories[i];
+      const intervals = traj.time_intervals;
+      let now = currTime - (traj.timeOffset || 0);
+
+      // the time interval will never start in the future
+      if (!intervals.length) {
+        // console.log('traj:', traj);
+        // eslint-disable-next-line no-continue
+        continue;
+      }
+      if (intervals[0][0] > now) {
+        [[now]] = intervals;
+      }
+
+      // find adjacent times in the interval list
+      let j = 0;
+      let start = 0;
+      let end = 0;
+      let startFrac = 0;
+      let endFrac = 0;
+      for (j = 0; j < intervals.length - 1; j += 1) {
+        [start, startFrac] = intervals[j];
+        [end, endFrac] = intervals[j + 1];
+
+        if (start <= now && now <= end) {
+          break;
+        } else {
+          start = null;
+          end = null;
+        }
+      }
+
+      if (start && end) {
+        // interpolate position based on the temporal fraction
+        const timeFrac = Math.min((now - start) / (end - start), 1);
+        const geomFrac = this.interpolate
+          ? timeFrac * (endFrac - startFrac) + startFrac
+          : 0;
+
+        traj.coordinate = traj.geom.getCoordinateAt(geomFrac);
+        geojson.features.push({
+          type: 'Feature',
+          geometry: {
+            type: 'Point',
+            coordinates: toLonLat(traj.coordinate),
+          },
+        });
+        /* const px = this.map.getPixelFromCoordinate(traj.coordinate);
+        const vehicleImg = this.style(traj, this.map.getView().getResolution());
+
+        this.canvasContext.drawImage(
+          vehicleImg,
+          px[0] - vehicleImg.width / 2,
+          px[1] - vehicleImg.width / 2,
+        ); */
+      } else {
+        this.removeTrajectory(traj.id);
+      }
+    }
+    return geojson;
   }
 
   /**
