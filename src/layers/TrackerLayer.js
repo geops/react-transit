9ab4083dcc5cb7d1dfe8d3dcba4703184fbd611c
edellucien <edellucien@gmail.com>
2020-05-12 08:56:35 +0200

    Add custom property to identify tracker layers

--- src/layers/TrackerLayer.js
+++ src/layers/TrackerLayer.js
@@ -71,6 +71,10 @@ class TrackerLayer extends Layer {
     if (options.onClick) {
       this.onClick(options.onClick);
     }
+
+    // Custom property for duck typing since `instanceof` is not working
+    // when the instance was created on different bundles.
+    this.isTrackerLayer = true;
   }
 
   /**
